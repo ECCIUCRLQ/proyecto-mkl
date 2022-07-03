1f9051bce1429cf4ba9ac4f0c20707dbf15d4f1c
lucy <andres.camachohoppe>
2022-07-03 13:30:59 -0600

    Device

--- modules/camara-comp/camara_comp.py
+++ modules/camara-comp/camara_comp.py
@@ -1,18 +1,12 @@
-# This Software is part of Simics. The rights to copy, distribute,
-# modify, or otherwise make use of this Software may be licensed only
-# pursuant to the terms of an applicable license agreement.
-# 
-# Copyright 2010-2022 Intel Corporation
+# empty_component.py - sample code for a Simics configuration component
+# Use this file as a skeleton for your own component implementations.
 
-# <add id="sample_components.py" label="sample_components.py">
-# <insert-until text="# END sample_components.py"/>
-# </add>
 import simics
-from comp import StandardComponent, SimpleConfigAttribute, Interface
+from comp import (StandardComponent, SimpleConfigAttribute, Interface)
 
-class sample_pci_card(StandardComponent):
-    """A sample component containing a sample PCI device."""
-    _class_desc = "Una camara PCI"
+class camara_comp(StandardComponent):
+    """instantiates all devices for the pci camara"""
+    _class_desc = "PCI Camara"
     _help_categories = ('PCI',)
 
     def setup(self):
@@ -22,33 +16,27 @@ class sample_pci_card(StandardComponent):
         self.add_connectors()
 
     def add_objects(self):
-        sd = self.add_pre_obj('dev', 'camara-pci')
-        sd.int_attr = self.integer_attribute.val
-
+        sd = self.add_pre_obj('dev', 'pci_camara')
+        
     def add_connectors(self):
-        self.add_connector(slot = 'pci_bus', type = 'pci-bus',
-                           hotpluggable = True, required = False, multi = False,
+        self.add_connector(slot = 'pci_bus', type = 'pci-bus', hotpluggable = True, required = False, multi = False,
                            direction = simics.Sim_Connector_Direction_Up)
-
+           
+    
     class basename(StandardComponent.basename):
         """The default name for the created component"""
-        val = "camara_comp"
-
-    class integer_attribute(SimpleConfigAttribute(None, 'i',
-                                                  simics.Sim_Attr_Required)):
-        """Example integer attribute."""
+        val = "pci_camara"
+                        
 
     class component_connector(Interface):
         """Uses connector for handling connections between components."""
         def get_check_data(self, cnt):
             return []
         def get_connect_data(self, cnt):
-            return [[[0, self._up.get_slot('sample_dev')]]]
+            return [[[0, self._up.get_slot('dev')]]]
         def check(self, cnt, attr):
             return True
         def connect(self, cnt, attr):
-            self._up.get_slot('sample_dev').pci_bus = attr[1]
+            self._up.get_slot('dev').pci_bus = attr[1]
         def disconnect(self, cnt):
-            self._up.get_slot('sample_dev').pci_bus = None
-
-# END sample_components.py
+            self._up.get_slot('dev').pci_bus = None
--- modules/camara-comp/module_load.py
+++ modules/camara-comp/module_load.py
@@ -1,8 +1,2 @@
-# This Software is part of Simics. The rights to copy, distribute,
-# modify, or otherwise make use of this Software may be licensed only
-# pursuant to the terms of an applicable license agreement.
-#
-# Copyright 2014-2022 Intel Corporation
-
 from . import camara_comp
-camara_comp.sample_pci_card.register()
+camara_comp.camara_comp.register()
